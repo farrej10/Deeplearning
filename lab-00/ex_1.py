90b300d915ca608b467a8d8d187e08d06abed7c0
farrej10 <farrej10@tcd.4c16>
2018-09-14 09:43:27 +0000

    added two ex files

--- lab-00/ex_1.py
+++ lab-00/ex_1.py
@@ -5,20 +5,22 @@
 # Ucomment the function below, and then add colons and fix the
 # indenting so that it returns 'None' if y is 0, and x/y otherwise.
 
-# def safe_div(x, y)
-# if (y == 0)
-# return None
-# return x / y
+def safe_div(x, y)
+    if (y == 0)
+    return None
+    return x / y
 
 
 # Fix this function so that it returns the sum of the three
 # parameters.
 def compute_sum(a, b, c):
-    return a + b - c
+    return a + b + c
 
 
 # Write a function called 'compute_product' which computes the product
 # of three numbers.
+def compute_product(a,b,c):
+    return a * b * c
 
 
 
