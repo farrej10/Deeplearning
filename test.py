57fee87787cb3b3244ca1c6884e586c27afcb1b5
farrej10 <farrej10@tcd.4c16>
2018-09-14 09:32:18 +0000

    added test file.

---
+++ test.py
@@ -0,0 +1,13 @@
+def say_stuff():
+    print("some day")
+    print("when the world is cold")
+    print("i will feel a glow")
+    print("just thinking of you")
+    print("and the way you look tonight")
+    print("this is added")
+
+
+def say_hello(x):
+    print("Hello, " + x + "!")
+
+
